7fbb1611fcc16ed9c94d2417e947d7ff126707d0
byrontorres26 <byrontorres@unitec.edu>
2026-09-12 18:14:58 -0600

    Se migro el acceso a productos, el estado local de la rutina quedo como estaba

--- MyRoutineApp/src/screens/Routines.tsx
+++ MyRoutineApp/src/screens/Routines.tsx
@@ -4,8 +4,9 @@ import { Ionicons } from "@expo/vector-icons";
 import ScreenWrapper from "../components/ScreenWrapper";
 import SectionTitle from "../components/SectionTitle";
 import { useTheme } from "../contexts/ThemeContext";
-import { useSkincare } from "../contexts/SkincareContext";
 import { CATEGORY_LABELS, Routine } from "../utils/types/Skincare";
+import { useAppSelector } from '../store/hooks';
+
 
 type RoutineSectionProps = {
   title: string;
@@ -24,7 +25,7 @@ function RoutineSection({
   onAdd,
   onRemove,
 }: RoutineSectionProps) {
-  const { products } = useSkincare();
+ const products = useAppSelector(state => state.skincare.products);
   const { colors } = useTheme();
 
   const routineProducts = productIds
@@ -113,7 +114,7 @@ function RoutineSection({
 }
 
 export default function Routines() {
-  const { products } = useSkincare();
+  const products = useAppSelector(state => state.skincare.products);
   const { colors } = useTheme();
 
   // EJERCICIO: reemplaza este estado local por Redux
